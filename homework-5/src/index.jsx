93e5013d81725066b200e50bae11c372509de77b
Yevgenii <yevhenii.katsimon@gmail.com>
2024-10-22 12:15:39 +0300

    add react comp

--- homework-5/src/index.jsx
+++ homework-5/src/index.jsx
@@ -6,7 +6,7 @@ import App from "./components/App.jsx";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
